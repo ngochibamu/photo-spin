2cbd82dd17a7b0aa047e5fbc60f8d753d8529546
Ngonidzashe Chibamu <ngoni.chibamu@gmail.com>
2015-08-20 08:29:00 +0200

    Added controller

--- src/main/java/com/chibamu/photospin/controllers/IndexController.java
+++ src/main/java/com/chibamu/photospin/controllers/IndexController.java
@@ -1,8 +1,17 @@
 package com.chibamu.photospin.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping(value ="/")
 public class IndexController {
 	
+	@RequestMapping (value = "welcome", method = RequestMethod.GET)
+	public ModelAndView welcome(){
+		ModelAndView modelView = new ModelAndView("welcome");
+		return modelView;
+	}
 }
